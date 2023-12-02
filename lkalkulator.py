f5a2b5a05449ef7ac6108cbe35dc07de24c20041
ItsaPlant <83190454+ItsaPlant@users.noreply.github.com>
2023-12-02 14:26:37 +0100

    ukąplikowanę

--- lkalkulator.py
+++ lkalkulator.py
@@ -1,6 +1,14 @@
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
 
+def add(n, m):
+    return n + m
+def sub(n, m):
+    return n - m
+def mult(n, m):
+    return n * m
+def div(n, m):
+    return n / m
 actions = {
     '1': 'Dodawanie',
     '2': 'Odejmowanie',
@@ -8,18 +16,18 @@ actions = {
     '4': 'Mnożenie'
 }
 action = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
-arg_1 = input('number 1')
-arg_2 = input('number 2')
-if type(arg_1) and type(arg_2) is not (int or float):
-    logging.info(f'nan: {arg_1, arg_2}')
+n = input('number 1')
+m = input('number 2')
+if type(n) and type(m) is not (int or float):
+    logging.info(f'nan: {n, m}')
     exit()
 if action == '1':
-    sol = arg_1 + arg_2
+    sol = add(n, m)
 elif action == '2':
-    sol = arg_1 - arg_2
+    sol = sub(n, m)
 elif action == '3':
-    sol = arg_1 * arg_2
+    sol = mult(n, m)
 elif action == '4':
-    sol = arg_1 / arg_2
-logging.info(f"{actions[action]} of: {arg_1} and {arg_2}")
+    sol = div(n, m)
+logging.info(f"{actions[action]} of: {n} and {m}")
 print(sol)
