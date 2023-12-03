897463a15fa3d1a5c9245143139dda92a8a2f1bf
ItsaPlant <83190454+ItsaPlant@users.noreply.github.com>
2023-12-03 21:11:49 +0100

    add if __main__

--- lkalkulator.py
+++ lkalkulator.py
@@ -24,24 +24,27 @@ actions = {
     '3': 'Dzielenie',
     '4': 'Mnożenie'
 }
-action = input(
-    "Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
-n = input('number 1')
-m = input('number 2')
-try:
-    n = float(n)
-    m = float(m)
-except:
-    logging.info(f'nan: {n, m}')
-    exit()
 
-if action == '1':
-    sol = add(n, m)
-elif action == '2':
-    sol = sub(n, m)
-elif action == '3':
-    sol = mult(n, m)
-elif action == '4':
-    sol = div(n, m)
-logging.info(f"{actions[action]} of: {n} and {m}")
-print(sol)
+
+if __name__ == "__main__":
+    action = input(
+        "Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
+    n = input('number 1')
+    m = input('number 2')
+    try:
+        n = float(n)
+        m = float(m)
+    except:
+        logging.info(f'nan: {n, m}')
+        exit()
+
+    if action == '1':
+        sol = add(n, m)
+    elif action == '2':
+        sol = sub(n, m)
+    elif action == '3':
+        sol = mult(n, m)
+    elif action == '4':
+        sol = div(n, m)
+    logging.info(f"{actions[action]} of: {n} and {m}")
+    print(sol)
