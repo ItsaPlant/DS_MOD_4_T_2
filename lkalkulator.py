3a23f3bb264c281684a8deabceb083c316a5a046
ItsaPlant <83190454+ItsaPlant@users.noreply.github.com>
2023-12-04 00:16:06 +0100

    Update elif/else and input spaces

--- lkalkulator.py
+++ lkalkulator.py
@@ -28,9 +28,9 @@ actions = {
 
 if __name__ == "__main__":
     action = input(
-        "Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
-    n = input('number 1')
-    m = input('number 2')
+        "Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
+    n = input('number 1: ')
+    m = input('number 2: ')
     try:
         n = float(n)
         m = float(m)
@@ -46,5 +46,9 @@ if __name__ == "__main__":
         sol = mult(n, m)
     elif action == '4':
         sol = div(n, m)
+    else:
+        logging.info(f'Action {action} not supported. Abort program.')
+        exit()
+        
     logging.info(f"{actions[action]} of: {n} and {m}")
     print(sol)
